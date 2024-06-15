cc0930e0ab85540604745a088c80ee2360e87fb6
Нодир Асомов <nasomov93@gmail.com>
2024-06-15 07:21:18 +0500

    queue

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,33 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> first = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<String> last = new PriorityQueue<>(Comparator.reverseOrder());
+
+        if (!(firstList.isEmpty() && secondList.isEmpty())) {
+
+            if (!firstList.isEmpty()) {
+                for (String i : firstList) {
+                    first.add(i);
+                }
+            }
+
+            if (!secondList.isEmpty()) {
+                for (String i : secondList) {
+                    first.add(i);
+                }
+            }
+
+            int size = first.size();
+            for (int i = 0; i < size; i++) {
+                last.add(first.poll());
+            }
+        }
+
+        return last;
     }
 }
